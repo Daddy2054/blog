f5f726840ea2a40542d537cdfa784fd45a1f509a
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-12-28 20:26:48 +0100

    seo-friendly urls

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,28 +1,32 @@
 from django.conf import settings
 from django.utils import timezone
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
 
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return (
-            super().get_queryset().filter(status=Post.Status.PUBLISHED)
-        )
-    
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
 
     class Status(models.TextChoices):
-        DRAFT = 'DF', 'Draft'
-        PUBLISHED = 'PB', 'Published'
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(
+        max_length=250,
+        unique_for_date="publish",
+    )
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name='blog_posts'
+        related_name="blog_posts",
     )
 
     body = models.TextField()
@@ -33,17 +37,28 @@ class Post(models.Model):
     status = models.CharField(
         max_length=2,
         choices=Status,
-        default=Status.DRAFT
+        default=Status.DRAFT,
     )
 
-    objects = models.Manager() # The default manager.
-    published = PublishedManager() # Our custom manager.
-    
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
+
     class Meta:
-        ordering = ['-publish']
+        ordering = ["-publish"]
         indexes = [
-            models.Index(fields=['-publish']),
+            models.Index(fields=["-publish"]),
         ]
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            "blog:post_detail",
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug,
+            ],
+        )

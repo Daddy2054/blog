dc47f2b20936b0ebc13e72a6107bee45a6e79678
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-12-30 23:42:07 +0100

    sitemap with tags

--- mysite/blog/sitemaps.py
+++ mysite/blog/sitemaps.py
@@ -1,10 +1,11 @@
 from django.contrib.sitemaps import Sitemap
-
+from taggit.models import Tag
+from django.urls import reverse
 from blog.models import Post
 
 
 class PostSitemap(Sitemap):
-    changefreq = 'weekly'
+    changefreq = "weekly"
     priority = 0.9
 
     def items(self):
@@ -12,3 +13,28 @@ class PostSitemap(Sitemap):
 
     def lastmod(self, obj):
         return obj.updated
+
+
+class TagSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+
+    def items(self):
+        # Get only tags that are actually used in posts
+        return Tag.objects.filter(post__status="PB").distinct()
+
+    def location(self, obj):
+        return reverse("blog:post_list_by_tag", args=[obj.slug])
+
+    # def lastmod(self, obj):
+    #     # Get the most recent post for this tag
+    #     return obj.post_set.filter(status="PB").latest("updated").updated
+
+
+def lastmod(self, obj):
+    # Get the most recent post for this tag using taggit's reverse relation
+    return (
+        obj.taggit_taggeditem_items.filter(content_object__status="PB")
+        .latest("content_object__updated")
+        .content_object.updated
+    )
